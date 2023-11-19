e2c309719d68d8381ebb7af52bc78d336de7cadd
Alfonso Dominguez <aldoan87@gmail.com>
2023-11-18 22:52:21 -0500

    - Create formulary - Handle onSubmit

--- src/components/AdditionalInfoForm/AdditionalInfoForm.tsx
+++ src/components/AdditionalInfoForm/AdditionalInfoForm.tsx
@@ -1,37 +1,129 @@
 import { useState, useEffect } from 'react'
 import ProductService from '../../services/ProductService'
-import Product from '../../models/Product'
+
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+
+import Button from '@mui/material/Button'
 import './AdditionalInfoForm.css'
 
 function AdditionalInfoForm() {
 
-const [products, setProducts] = useState<Product[]>([]);
+const [products, setProducts] = useState<string[]>([])
+const [productSelectedForTags, setProductSelectedForTags] = useState<string[]>([])
+const [tags, setTags] = useState<string[]>([])
+const [topic, setTopic] = useState<string>('')
+const [topicInfo, setTopicInfo] = useState<string>('')
+const [productSelectedForInfo, setProductSelectedForInfo] = useState<string[]>([])
+const [productsInfo, setProductsInfo] = useState<string>('')
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetchedProducts: Product[] = await ProductService.getProducts();
-        setProducts(fetchedProducts);
+        const fetchedProducts: string[] = await ProductService.getProducts()
+        setProducts(fetchedProducts)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
+
+  const handleSubmit =  (evt:React.FormEvent) => {
+    evt.preventDefault()
+    
+    console.log("Products Selected For Tags: ", productSelectedForTags)
+    console.log("Tags: ", tags)
+    console.log("Topic: ", topic)
+    console.log("Topic Info: ", topicInfo)
+    console.log("Products Selected For Info: ", productSelectedForInfo)
+    console.log("productsInfo: ", productsInfo)
+
+  }
 
   return (
-    <>
-
+    <form 
+      action=''
+      method='POST'
+      onSubmit={ evt => handleSubmit(evt) } >
       <h1>Aditional Information</h1>
-     <ul>
-      {
-        products.map((product) => <li>{product.displayTitle}</li>)
-      }
-     </ul>
-      
+      <p>Improve  your customer support by supplementing Wizybot's knowledge with specific product info, policies and unique details.</p>
+      <h3>Add product tags</h3>
+      <Autocomplete
+        multiple
+        id="product-select-for-tags"
+        options={products}
+        onChange={(event, newValue: string[]) => { setProductSelectedForTags(newValue) }}
+        getOptionLabel={(option) => option}
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField 
+            {...params} 
+            label="Products" 
+            placeholder="Select products..."/>
+        )}
+        renderOption={(props, option, state) => {
+          const key = `listItem-${state.index}`
+          return (<li {...props} key={key}> {option} </li>)
+        }}
+      />
 
-    </>
+      <Autocomplete
+        multiple
+        id="product-tags"
+        freeSolo
+        options={[]}
+        onChange={(event, newValue: string[]) => { setTags(newValue)}}
+        renderInput={(params) => <TextField {...params} label="Tags" />}
+      />
+
+      <h3>Add topic info</h3>
+      <TextField 
+        id="topic" 
+        label="Topic" 
+        variant="outlined" 
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setTopic(event.target.value)}}
+      />
+      <TextField
+          id="topic-info"
+          label="Topic Info"
+          multiline
+          minRows={3}
+          maxRows={7}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setTopicInfo(event.target.value)}}
+      />  
+
+      <h3>Add products info</h3>
+      <Autocomplete
+        multiple
+        id="product-select-for-info"
+        options={products}
+        onChange={(event, newValue: string[]) => {setProductSelectedForInfo(newValue)}}
+        getOptionLabel={(option) => option}
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField {...params} label="Products" placeholder="Select products..." />
+        )}
+        renderOption={(props, option, state) => {
+          const key = `listItem-${state.index}`
+          return (
+            <li {...props} key={key}>
+              {option}
+            </li>
+          )
+        }}
+      />
+      <TextField
+          id="products-info"
+          label="Products Info"
+          multiline
+          minRows={3}
+          maxRows={7}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setProductsInfo(event.target.value)}}
+      /> 
+      <Button variant="contained" type='submit' >Save</Button>
+    </form >
   )
 }
 
--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -4,10 +4,10 @@ import ProductRepository from '../repositories/ProductRepository'
 import Product from '../models/Product'
 
 class ProductService {
-  async getProducts(): Promise<Product[]> {
+  async getProducts(): Promise<string[]> {
     try {
       const productItems: Product[] = await ProductRepository.fetchProducts();
-      return productItems;
+      return productItems.map(product => product.displayTitle);
     } catch (error) {
       throw new Error(`Error fetching items: ${error.message}`);
     }
