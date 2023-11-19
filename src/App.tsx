7df0c771eec51532080edd949486aba7878ef1c8
Alfonso Dominguez <aldoan87@gmail.com>
2023-11-19 01:29:58 -0500

    - Add Header - Add Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
 import Container from '@mui/material/Container'
 import AdditionalInfoForm from './components/AdditionalInfoForm/AdditionalInfoForm'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar'
+import WizyBotLogo from './components/WizyBotLogo/WizyBotLogo';
 
 function App() {
   return (
     <>
+       <AppBar position="static" className='header'>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <WizyBotLogo />
+          </Toolbar>
+        </Container>
+      </AppBar>
       <Container>
         <AdditionalInfoForm />
       </Container>
---
+++ src/components/WizyBotLogo/WizyBotLogo.tsx
@@ -0,0 +1,7 @@
+import logo from '../../assets/images/Wizybot-Logo.webp'
+
+function WizyBotLogo() {
+    return <img src={logo} alt="Wizybot Logo" className='wizibot-log' />
+}
+
+export default WizyBotLogo
